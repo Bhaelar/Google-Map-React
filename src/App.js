cfd2f3a5c433bb208ec259e59199b89fcdca75df
Bhaelar <okenlabasit@gmail.com>
2020-06-21 22:23:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyMapComponent = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap defaultZoom={12} defaultCenter={{ lat: props.lat, lng: props.lon }}>
+      {props.isMarkerShown && <Marker position={{ lat: props.lat, lng: props.lon }} />}
+    </GoogleMap>
+  ))
+);
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lat: null,
+      lon: null,
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => this.setState ({
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    }));
+
+    
+  }
+
+  
+
+	render () {
+		return (
+			<div className="App">
+				<div className="text-center">
+					<h1>GOOGLE MAPS API with REACT JS</h1>
+				</div>
+				<div className="container" style={{margin: `100px auto`}}>
+					<div className="row">
+						<div className="col-12">
+							
+							<div>
+								<MyMapComponent
+                  isMarkerShown
+                  lat={this.state.lat}
+                  lon={this.state.lon}
+									googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAsg_JJLuQmzsMYVZDpPR8wToxMLyziv1w&v=3.exp&libraries=geometry,drawing,places"
+									loadingElement={<div style={{ height: `100%` }} />}
+									containerElement={<div style={{ height: `400px` }} />}
+									mapElement={<div style={{ height: `100%` }} />}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+        <div className="footer">
+          <p>Developed by <a href="http://bhaelar.github.io" target="__blank">Bhaelar</a></p>
+        </div>
+			</div>
+		);
+	}
 }
 
 export default App;
